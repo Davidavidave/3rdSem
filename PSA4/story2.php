56a973cd882d5e1e34ae943ca5a527b017b6f7b9
Davidavidave <111590576+Davidavidave@users.noreply.github.com>
2026-05-18 16:22:44 +0800

    Delete PSA4 directory

--- PSA4/story2.php
+++
@@ -1,4 +0,0 @@
-
-<h2>Jay: The Artist</h2>
-<img src="jay.jpg" alt="Bianca painting">
-<p>She is Bianca one of my most talented friend singer she is very kind also like awra </p>
--- PSA4/story5.php
+++
@@ -1,3 +0,0 @@
-<h2>Jay: The Explorer</h2>
-<img src="step.webp" alt="Ethan hiking mountains">
-<p>He is stephen Curry a 3 poinbter basketball player in tha NBA.   </p>
