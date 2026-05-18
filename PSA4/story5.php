5072a87014dc75aa7f5990500a9dd88a30c88005
Davidavidave <stephendavealvarezblanza@gmail.com>
2026-05-18 17:27:18 +0800

    new tezst

--- PSA4/story5.php
+++ PSA4/story5.php
@@ -1,5 +1,5 @@
 <h2>The Baker's Secret</h2>
-<img src="https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=600" alt="Cozy Bakery">
+<img src="" alt="Cozy Bakery">
 <p>
     hidave
 </p>
